d9cda1d28cbc0b8085faa654377526dc7170fc3f
MatyasHroch <matyas.hroch01@upol.cz>
2023-11-24 13:06:12 +0100

    set up comunication (cors mostly)

--- index.ts
+++ index.ts
@@ -1,14 +1,24 @@
 import express, { Express, Request, Response, Application } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 //For env File
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
+app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to Express & TypeScript Server");
+  res.send({ message: "Welcome to Express & TypeScript Server" });
+});
+
+app.get("/api/:message", (req: Request, res: Response) => {
+  res.send({ message: "Welcome to " + req.params.message });
+});
+
+app.post("/", (req: Request, res: Response) => {
+  res.send("Welcome to Express & TypeScript Server POST");
 });
 
 app.listen(port, () => {
